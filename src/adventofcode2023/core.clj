7446bc7f3bef07b1e4166da9ab7034ee08e22f18
Tycho Lamerigts <@>
2023-12-06 20:47:22 +0100

    Add day 1

--- src/adventofcode2023/core.clj
+++ src/adventofcode2023/core.clj
@@ -1,25 +1,25 @@
 (ns adventofcode2023.core
-    (:require [clojure.java.io :as io]
-      [clojure.string :as str]
-      [clojure.set :as set]
-      [clojure.data.priority-map :refer [priority-map]]
-      [clojure.walk :as w]
-      [clojure.zip :as z]
-      [clojure.math.combinatorics :as combo]
-      [clojure.pprint :as pp]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]
+            [clojure.data.priority-map :refer [priority-map]]
+            [clojure.walk :as w]
+            [clojure.zip :as z]
+            [clojure.math.combinatorics :as combo]
+            [clojure.pprint :as pp]))
 
 (defn inputs
-      "read file from resources, apply f to each line, return seq"
-      [name f]
-      (map f (str/split-lines (slurp (io/resource name)))))
+  "read file from resources, apply f to each line, return seq"
+  [name f]
+  (map f (str/split-lines (slurp (io/resource name)))))
 
 (defn parse-int
-      [s]
-      (Integer/parseInt s))
+  [s]
+  (Integer/parseInt s))
 
 (defn parse-ints-csv
-      [s]
-      (map parse-int (str/split s #",")))
+  [s]
+  (map parse-int (str/split s #",")))
 
 (defn day1-1
   "--- Day 1: Trebuchet?! ---"
@@ -29,3 +29,19 @@
          (map #(str/replace % #"[^0-9]" ""))
          (map #(parse-int (str (first %) (last %))))
          (apply +))))
+
+(defn map-numbers [pairs s]
+  (let [first-digit (apply min-key #(or (str/index-of s (second %)) Integer/MAX_VALUE) pairs)
+        last-digit (apply max-key #(or (str/last-index-of s (second %)) Integer/MIN_VALUE) pairs)]
+    (parse-int (str (first first-digit) (first last-digit)))))
+
+(defn day1-2
+  "--- Day 1 Part Two: Trebuchet?! ---"
+  [name]
+  (let [v (inputs name identity)
+        num-str-pairs (concat
+                        (map-indexed vector (map str (range 10)))
+                        (map-indexed vector ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]))
+        nums (map #(map-numbers num-str-pairs %) v)]
+    (reduce + nums)))
+
--- test/adventofcode2023/core_test.clj
+++ test/adventofcode2023/core_test.clj
@@ -1,7 +1,14 @@
 (ns adventofcode2023.core-test
-    (:require [clojure.test :refer :all]
-      [adventofcode2023.core :refer :all]
-      [clojure.zip :as z]))
+  (:require [clojure.test :refer :all]
+            [adventofcode2023.core :refer :all]
+            [clojure.zip :as z]))
 
 (deftest day1-1-test
-         (is (= (day1-1 "1-example.txt") 142)))
+  (is (= (day1-1 "1-example.txt") 142))
+  (is (= (day1-1 "1.txt") 55488))
+  )
+
+(deftest day1-2-test
+  (is (= (day1-2 "1-2-example.txt") 281))
+  (is (= (day1-2 "1.txt") 55614))
+  )
